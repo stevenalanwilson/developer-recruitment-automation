6bc6264a7f298a312f6c249e9a8bc60c25100ae0
Steve Wilson <steve.wilson@digtial.justice.gov.uk>
2022-02-13 13:23:23 +0000

    Refactor index.js
    - Use config object for templateowner and templaterepo
    - Use config for Candidate details
    - Use util function GetLastCandidateFromList to get last candiate

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const { client } = require('./client');
 const { utils } = require('./utils')
 const { ManageDeveloperTechTests } = require('./service')
 
+const candidates = require('./config/candidates.json');
+const technicalTextRepoConfig = require('./config/technicalTextRepoConfig.json');
+
 const service = new ManageDeveloperTechTests(client);
 
 const config = {
-    contributor: 'candidatename',
-    templateowner: 'repo-owner', 
-    templaterepo: 'repo-name' 
+    contributor: utils.GetLastCandidateFromList(candidates),
+    templateowner: technicalTextRepoConfig.templateowner,
+    templaterepo: technicalTextRepoConfig.templaterepo
 }
 
 service.setupCandidateTechtest(config, utils)
-    
