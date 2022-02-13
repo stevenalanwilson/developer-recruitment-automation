05af40cbf3d7867093d4ef54ae45f958189aa326
Steve Wilson <steve.wilson@digtial.justice.gov.uk>
2022-02-13 13:20:20 +0000

    Add GetLastCandidateFromList functino to Utils

--- __tests__/utils.spec.js
+++ __tests__/utils.spec.js
@@ -5,4 +5,26 @@ describe('Utils', () => {
     const repository_name = utils.generateRepoName('johnsmith01');
     expect(repository_name).toBe('johnsmith01-moj-tech-test');
   });
+
+  test('Get the last candidate from a list', async () => {
+    ListOfCandidates = [
+      {
+        "id": 0,
+        "Name": "John Wilson",
+        "GithubUsername": "candidateGithubUsername"
+      },
+      {
+        "id": 0,
+        "Name": "Steven Wilson",
+        "GithubUsername": "candidateGithubUsername"
+      },
+      {
+        "id": 0,
+        "Name": "Samuel Wilson",
+        "GithubUsername": "candidateGithubUsername"
+      },
+    ]
+    const lastCandidate = utils.GetLastCandidateFromList(ListOfCandidates);
+    expect(lastCandidate.Name).toBe('Samuel Wilson');
+  });
 });
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 const utils = {
-    generateRepoName: (name) => `${name.replace(/\s+/g, '-').toLowerCase()}-moj-tech-test`
+    generateRepoName: (name) => `${name.replace(/\s+/g, '-').toLowerCase()}-moj-tech-test`,
+
+    GetLastCandidateFromList: (listOfCandidates) => listOfCandidates[listOfCandidates.length - 1]
 }
 
 module.exports = {
